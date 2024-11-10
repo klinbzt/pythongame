c29c119dedbf574c3d1c9ebb24eec7d296d7d179
GarouLevelThreat <gamedevmihnea@gmail.com>
2024-11-10 17:29:56 +0200

    Created a more detailed structure of the project and provided guidelines for each section.

---
+++ src/settings.py
@@ -0,0 +1,11 @@
+# Here we declare and initialize consts
+
+# Screen dimensions
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FPS = 60
+
+# Colors
+BLACK = (0, 0, 0)
+
+# Player settings ( base settings can be altered later by power-ups, planet gravity, etc. )
